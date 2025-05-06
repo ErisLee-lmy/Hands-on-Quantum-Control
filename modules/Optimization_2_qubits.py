37084e649b4ff98d3f1a5d935190e3fbba2d3d45
“ErisLee-lmy” <3216992290@qq.com>
2025-05-06 22:37:42 +0800

    update

--- modules/Optimization_2_qubits.py
+++ modules/Optimization_2_qubits.py
@@ -94,7 +94,7 @@ class Optimalization_2_qubits:
                 best_phi = result[1]
         return best_result, best_phi
     
-    def optimize_test(self,total,hist=False,save=False,report = True):
+    def optimize_test(self,total,hist=False,save=False,report_process = True):
         # try to find the optimal phi from several random phi and get the best result
         print(f"Start optimizing with T = {self.T}...")
         T_sample = self.T
@@ -110,7 +110,7 @@ class Optimalization_2_qubits:
             if result[0] < 0.1:
                 count_optimal += 1
             results.append(result[0]) # Append the minimum fidelity to the results list
-            if report:
+            if report_process:
                 
                 print(f" progress:{count}/{total}, probility: {count_optimal/count},minimum result: {np.min(results)}")
         
