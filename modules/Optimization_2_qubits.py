5bed72b5e8bdd2afeaf80c390e34ce705a1a6b60
“ErisLee-lmy” <3216992290@qq.com>
2025-06-01 11:24:10 +0800

    update

--- modules/Optimization_2_qubits.py
+++ modules/Optimization_2_qubits.py
@@ -1,7 +1,6 @@
 # from qutip import * # qutip == 5.0.4
 import numpy as np
 from scipy.linalg import expm
-
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 import warnings
@@ -16,13 +15,13 @@ dim = 4
 
 basis_state = np.zeros((dim, dim), dtype=complex) # Initialize basis states as a 2D array
 # in the basis of |10>, |0r>, |11>, |W>=(|1r>+|r1>)/sqrt2
-for i in range(dim):
+for i in range(dim): 
     basis_state[i, i] = 1 # Set diagonal elements to 1 for basis states
 
 
 sqrt2 = np.sqrt(2)
 
-Omega_max = 1 # fix maximum Omega
+Omega_max = 100.0   # fix maximum Omega
 T_max = 1000.0 # maximum time
 T_list = np.linspace(0, T_max, 100)
 
@@ -33,23 +32,24 @@ psi_in = np.array([1, 0, 1, 0]) # Convert to numpy array for consistency
 
 class qubits2:
     # input T get optimal 1-F and optimized phi
-    def __init__(self, T, N =99, psi_in = psi_in, dim = 4):
+    def __init__(self, T, N =99, psi_in = psi_in, dim = 4, Omega_max = Omega_max):
         self.T = T
         self.N = N
-        self.dt = T / N # time step
+        self.Omega_max = Omega_max
         self.dim = dim
         
         self.psi_in = psi_in
         
         self.parameter = np.random.uniform(0, 2*np.pi, N+1) # Random initial parameters
-        self.phi = self.parameter[1:]
-        self.theta = self.parameter[0] # theta is not used in this case, but can be used for other purposes
+        
+        
 
         
     def get_H(self):
+        self.phi = self.parameter[1:]
         phi = self.phi
         
-        Omega = Omega_max * np.exp(1j*phi)# Global Pulse so Omega_1 = Omega_2 = Omega
+        Omega = self.Omega_max * np.exp(1j*phi)# Global Pulse so Omega_1 = Omega_2 = Omega
 
         H = np.zeros((4,4)) # Hamiltonian matrix
         # in the basis of |01>, |0r>,|11>,|W>=(|1r>+|r1>)/sqr2
@@ -67,7 +67,7 @@ class qubits2:
         return H_list # Return the list of Hamiltonian matrices
 
     def get_U(self):
-        dt = self.dt
+        dt = self.T / self.N # Time step size
 
         # Define the unitary operator
         H_list = self.get_H() # Get the Hamiltonian list
@@ -82,6 +82,8 @@ class qubits2:
         return U_total # Return the total unitary operator
 
     def get_fidelity(self):
+        
+        self.theta = self.parameter[0] 
         
         # use the average fidelity fomula
         U = self.get_U()
@@ -117,6 +119,8 @@ class qubits2:
 
 if __name__ == "__main__":
     # Test the optimization
+    import time
+    start_time = time.time()
     for T in [0.01, 1.0, 5.0, 10.0, 100.0, 1000.0]:
         print(f"Optimizing for T = {T:.2f}")
         T_sample = 0.01
@@ -125,6 +129,8 @@ if __name__ == "__main__":
         reuslt = sample.optimize()
         print(f"Optimized Loss: {reuslt[0]:.4f}")
         print(f"Optimized Theta: {reuslt[1][0]:.4f}")
+        print(f"Current time: {time.time() - start_time:.2f} seconds")
+
 
     
     
